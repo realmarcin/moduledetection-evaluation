3d685095a79197f8d42f02e5e137ad42157d290b
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-10-18 12:28:21 -0700

    method return

--- conf/paramexplo_blueprints.py
+++ conf/paramexplo_blueprints.py
@@ -607,4 +607,5 @@ def run_blueprints():
         methodparamsoi[method + "_auto"] = [param for param in methodparamsoi[method] if
                                             param not in methodparams_modulenumber[method]]
         methodparamsoi[method + "_auto"].append("cvi")
-        methodparamsoi
+
+    methodparamsoi
