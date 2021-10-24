7a873e9cbea7456a22e301a2388ca23d5c62b9de
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-10-24 12:51:19 -0700

    generate MAK

--- notebooks/run_and_eval.py
+++ notebooks/run_and_eval.py
@@ -48,8 +48,8 @@ datasetnames = [
 #method_name = "agglom" # this method runs very fast, and has the best performance among clustering methods
 #method_name = "ica_zscore" # this method runs very slow, but has approx. the highest performance in the benchmark
 #method_name = "spectral_biclust" # top biclustering method
-# method_name = "MAK"
-method_name = "meanshift"
+method_name = "MAK"
+#method_name = "meanshift"
 
 
 #blueprints = os.system("python conf/paramexplo_blueprints.py")
