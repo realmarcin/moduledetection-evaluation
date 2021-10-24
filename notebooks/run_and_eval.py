1757519952130a7af7eb7e0ed4fd36882fb3a993
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-10-24 12:27:53 -0700

    paths

--- notebooks/run_and_eval.py
+++ notebooks/run_and_eval.py
@@ -91,7 +91,7 @@ for datasetname in datasetnames:
     for setting_ix, methodsetting in enumerate(methodsettings):
         settingid = datasetname + "_" + str(setting_ix)
         settings.append({
-            "dataset_location": "conf/datasets/" + datasetname + ".json",
+            "dataset_location": "../conf/datasets/" + datasetname + ".json",
             "dataset_name": datasetname,
             "method_location": methodsetting["location"],
             "output_folder": "../results/" + methodblueprint["type"] + "/{settings_name}/{settingid}/".format(
@@ -103,7 +103,7 @@ json.dump(settings, open("../conf/settings/{settings_name}.json".format(settings
 # %%
 
 settings_dataset = pd.DataFrame(
-    [dict(settingid=setting["settingid"], **json.load(open("../" +setting["dataset_location"]))["params"]) for setting
+    [dict(settingid=setting["settingid"], **json.load(open(setting["dataset_location"]))["params"]) for setting
      in settings])
 settings_method = pd.DataFrame(
     [dict(settingid=setting["settingid"], **json.load(open(setting["method_location"]))["params"]) for setting
