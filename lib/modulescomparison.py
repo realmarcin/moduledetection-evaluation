0cf40722e715a93f9050eff8e41fbb7b4af0a6d8
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-11-04 16:44:34 -0700

    debug

--- lib/modulescomparison.py
+++ lib/modulescomparison.py
@@ -200,7 +200,7 @@ def modevalworker(setting, scores, baseline):
             knownmodules = Modules(json.load(open("../" + knownmodules_location)))
             #print(knownmodules)
             print("module set sizes: known :new "+str(len(knownmodules))+"\t"+str(len(modules)))
-            settingscores_goldstandard = modevalscorer(modules, knownmodules, baselineoi, regnet_name, knownmodules_name)
+            settingscores_goldstandard = modevalscorer(modules, knownmodules, regnet_name, knownmodules_name, baselineoi)
 
             settingscores_goldstandard["settingid"] = setting["settingid"]
 
